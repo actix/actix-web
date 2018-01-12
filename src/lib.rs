11342e4566396df3033d265fd884b7e99e3c51bb
Nikolay Kim <fafhrd91@gmail.com>
2018-01-11 18:49:30 -0800

    add link to gitter

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,9 @@
 //! ## Documentation
 //!
 //! * [User Guide](http://actix.github.io/actix-web/guide/)
-//! * Cargo package: [actix-web](https://crates.io/crates/actix-web)
+//! * [Chat on gitter](https://gitter.im/actix/actix)
 //! * [GitHub repository](https://github.com/actix/actix-web)
+//! * Cargo package: [actix-web](https://crates.io/crates/actix-web)
 //! * Supported Rust version: 1.20 or later
 //!
 //! ## Features
