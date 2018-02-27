7d5e3e98a57269a02e27e6aa1e840e65f5733a8e
pyros2097 <pyros2097@gmail.com>
2018-02-27 22:02:56 +0530

    improve juniper example

--- examples/juniper/src/main.rs
+++ examples/juniper/src/main.rs
@@ -1,8 +1,12 @@
 //! Actix web juniper example
 //!
-//! Juniper is a graphql framework implemetation for rust
+//! A simple example integrating juniper in actix-web
 extern crate serde;
 extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate lazy_static;
 #[macro_use]
 extern crate juniper;
 extern crate futures;
@@ -10,6 +14,7 @@ extern crate actix;
 extern crate actix_web;
 extern crate env_logger;
 
+use actix::*;
 use actix_web::*;
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
@@ -21,8 +26,35 @@ mod schema;
 use schema::Schema;
 use schema::create_schema;
 
+lazy_static! {
+    static ref SCHEMA: Schema = create_schema();
+}
+
 struct State {
-    schema: Schema,
+    executor: Addr<Syn, GraphQLExecutor>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GraphQLData(GraphQLRequest);
+
+impl Message for GraphQLData {
+    type Result = Result<String, Error>;
+}
+
+pub struct GraphQLExecutor;
+
+impl Actor for GraphQLExecutor {
+    type Context = SyncContext<Self>;
+}
+
+impl Handler<GraphQLData> for GraphQLExecutor {
+    type Result = Result<String, Error>;
+
+    fn handle(&mut self, msg: GraphQLData, _: &mut Self::Context) -> Self::Result {
+        let res = msg.0.execute(&SCHEMA, &());
+        let res_text = serde_json::to_string(&res)?;
+        Ok(res_text)
+    }
 }
 
 fn graphiql(_req: HttpRequest<State>) -> Result<HttpResponse>  {
@@ -33,11 +65,18 @@ fn graphiql(_req: HttpRequest<State>) -> Result<HttpResponse>  {
 }
 
 fn graphql(req: HttpRequest<State>) -> Box<Future<Item=HttpResponse, Error=Error>> {
+
     req.json()
         .from_err()
-        .and_then(move |val: GraphQLRequest| {
-            let response = val.execute(&req.state().schema, &());
-            Ok(httpcodes::HTTPOk.build().json(response)?)
+        .and_then(move |val: GraphQLData| {
+            req.state().executor.send(val)
+                .from_err()
+                .and_then(|res| {
+                    match res {
+                        Ok(user) => Ok(httpcodes::HTTPOk.build().body(user)?),
+                        Err(_) => Ok(httpcodes::HTTPInternalServerError.into())
+                    }
+                })
         })
         .responder()
 }
@@ -47,9 +86,13 @@ fn main() {
     let _ = env_logger::init();
     let sys = actix::System::new("juniper-example");
 
+    let addr = SyncArbiter::start(3, || {
+        GraphQLExecutor{}
+    });
+
     // Start http server
     let _addr = HttpServer::new(move || {
-        Application::with_state(State{schema: create_schema() })
+        Application::with_state(State{executor: addr.clone()})
             // enable logger
             .middleware(middleware::Logger::default())
             .resource("/graphql", |r| r.method(Method::POST).a(graphql))
