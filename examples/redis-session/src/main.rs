ca76dff5a70e78ab0bad2134084bae480d9249bd
Nikolay Kim <fafhrd91@gmail.com>
2018-04-10 13:21:54 -0700

    update redis example

--- examples/redis-session/src/main.rs
+++ examples/redis-session/src/main.rs
@@ -5,8 +5,8 @@ extern crate actix_web;
 extern crate actix_redis;
 extern crate env_logger;
 
-use actix_web::*;
-use actix_web::middleware::RequestSession;
+use actix_web::{server, App, HttpRequest, HttpResponse, Result};
+use actix_web::middleware::{Logger, SessionStorage, RequestSession};
 use actix_redis::RedisSessionBackend;
 
 
@@ -30,12 +30,12 @@ fn main() {
     env_logger::init();
     let sys = actix::System::new("basic-example");
 
-    HttpServer::new(
-        || Application::new()
+    server::new(
+        || App::new()
             // enable logger
-            .middleware(middleware::Logger::default())
+            .middleware(Logger::default())
             // cookie session middleware
-            .middleware(middleware::SessionStorage::new(
+            .middleware(SessionStorage::new(
                 RedisSessionBackend::new("127.0.0.1:6379", &[0; 32])
             ))
             // register simple route, handle all methods
