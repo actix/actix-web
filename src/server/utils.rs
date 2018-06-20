da237611cb429da5fb42ab9d6be52837d1de3dda
gnzlbg <gonzalobg88@gmail.com>
2018-06-20 13:14:53 +0200

    remove unnecessary use of unsafe in read_from_io

--- src/server/utils.rs
+++ src/server/utils.rs
@@ -1,5 +1,5 @@
 use bytes::{BufMut, BytesMut};
-use futures::{Async, Poll};
+use futures::Poll;
 use std::io;
 
 use super::IoStream;
@@ -10,22 +10,8 @@ const HW_BUFFER_SIZE: usize = 32_768;
 pub fn read_from_io<T: IoStream>(
     io: &mut T, buf: &mut BytesMut,
 ) -> Poll<usize, io::Error> {
-    unsafe {
-        if buf.remaining_mut() < LW_BUFFER_SIZE {
-            buf.reserve(HW_BUFFER_SIZE);
-        }
-        match io.read(buf.bytes_mut()) {
-            Ok(n) => {
-                buf.advance_mut(n);
-                Ok(Async::Ready(n))
-            }
-            Err(e) => {
-                if e.kind() == io::ErrorKind::WouldBlock {
-                    Ok(Async::NotReady)
-                } else {
-                    Err(e)
-                }
-            }
-        }
+    if buf.remaining_mut() < LW_BUFFER_SIZE {
+        buf.reserve(HW_BUFFER_SIZE);
     }
+    io.read_buf(buf)
 }
