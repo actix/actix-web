e680541e10aff1fc6a4d271ab308516e835a73a0
Franz Gregor <franz.gregor@tu-dresden.de>
2018-08-18 19:32:28 +0200

    Made extensions constructor public

--- src/extensions.rs
+++ src/extensions.rs
@@ -39,7 +39,7 @@ pub struct Extensions {
 impl Extensions {
     /// Create an empty `Extensions`.
     #[inline]
-    pub(crate) fn new() -> Extensions {
+    pub fn new() -> Extensions {
         Extensions {
             map: HashMap::default(),
         }
