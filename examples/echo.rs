4028f6f6fd63cad32636d5f1b461f372302c9af4
Ali Shirvani <aj.shirvani@gmail.com>
2018-11-28 09:42:04 +0330

    http crate removed, cargo fmt

--- examples/echo.rs
+++ examples/echo.rs
@@ -4,39 +4,37 @@ extern crate env_logger;
 
 extern crate actix_http;
 extern crate actix_net;
+extern crate bytes;
 extern crate futures;
 extern crate http;
-extern crate bytes;
 
-use actix_http::{h1, Response, Request};
-use bytes::Bytes;
+use actix_http::HttpMessage;
+use actix_http::{h1, Request, Response};
 use actix_net::server::Server;
 use actix_net::service::NewServiceExt;
+use bytes::Bytes;
 use futures::Future;
-use http::header::{HeaderValue};
-use actix_http::HttpMessage;
+use http::header::HeaderValue;
 use std::env;
 
 fn main() {
     env::set_var("RUST_LOG", "echo=info");
     env_logger::init();
 
-    Server::new().bind("echo", "127.0.0.1:8080", || {
-        h1::H1Service::build()
-            .client_timeout(1000)
-            .client_disconnect(1000)
-            .server_hostname("localhost")
-            .finish(|_req: Request| {
-                _req.body()
-                    .limit(512)
-                    .and_then(|bytes: Bytes| {
+    Server::new()
+        .bind("echo", "127.0.0.1:8080", || {
+            h1::H1Service::build()
+                .client_timeout(1000)
+                .client_disconnect(1000)
+                .server_hostname("localhost")
+                .finish(|_req: Request| {
+                    _req.body().limit(512).and_then(|bytes: Bytes| {
                         info!("request body: {:?}", bytes);
                         let mut res = Response::Ok();
                         res.header("x-head", HeaderValue::from_static("dummy value!"));
                         Ok(res.body(bytes))
                     })
-            })
-            .map(|_| ())
-    }).unwrap().run();
+                }).map(|_| ())
+        }).unwrap()
+        .run();
 }
-
--- examples/echo2.rs
+++ examples/echo2.rs
@@ -4,44 +4,39 @@ extern crate env_logger;
 
 extern crate actix_http;
 extern crate actix_net;
-extern crate futures;
-extern crate http;
 extern crate bytes;
+extern crate futures;
 
-use actix_http::{h1, Response, Request, Error};
-use bytes::Bytes;
+use actix_http::http::HeaderValue;
+use actix_http::HttpMessage;
+use actix_http::{h1, Error, Request, Response};
 use actix_net::server::Server;
 use actix_net::service::NewServiceExt;
+use bytes::Bytes;
 use futures::Future;
-use http::header::{HeaderValue};
-use actix_http::HttpMessage;
 use std::env;
 
-fn handle_request(_req: Request) -> impl Future<Item = Response, Error = Error>{
-    _req.body()
-        .limit(512)
-        .from_err()
-        .and_then(|bytes: Bytes| {
-            info!("request body: {:?}", bytes);
-            let mut res = Response::Ok();
-            res.header("x-head", HeaderValue::from_static("dummy value!"));
-            Ok(res.body(bytes))
-        })
+fn handle_request(_req: Request) -> impl Future<Item = Response, Error = Error> {
+    _req.body().limit(512).from_err().and_then(|bytes: Bytes| {
+        info!("request body: {:?}", bytes);
+        let mut res = Response::Ok();
+        res.header("x-head", HeaderValue::from_static("dummy value!"));
+        Ok(res.body(bytes))
+    })
 }
 
 fn main() {
     env::set_var("RUST_LOG", "echo=info");
     env_logger::init();
 
-    Server::new().bind("echo", "127.0.0.1:8080", || {
-        h1::H1Service::build()
-            .client_timeout(1000)
-            .client_disconnect(1000)
-            .server_hostname("localhost")
-            .finish(|_req: Request| {
-                handle_request(_req)
-            })
-        .map(|_| ())
-    }).unwrap().run();
+    Server::new()
+        .bind("echo", "127.0.0.1:8080", || {
+            h1::H1Service::build()
+                .client_timeout(1000)
+                .client_disconnect(1000)
+                .server_hostname("localhost")
+                .finish(|_req: Request| handle_request(_req))
+                .map(|_| ())
+        }).unwrap()
+        .run();
 }
-
