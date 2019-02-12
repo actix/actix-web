8d4ce0c956f6a81d0b8aec50ad220b352f12e5c0
Nikolay Kim <fafhrd91@gmail.com>
2019-02-12 11:09:58 -0800

    export PayloadStream

--- src/lib.rs
+++ src/lib.rs
@@ -95,7 +95,7 @@ pub use self::error::{Error, ResponseError, Result};
 pub use self::extensions::Extensions;
 pub use self::httpmessage::HttpMessage;
 pub use self::message::{Message, RequestHead, ResponseHead};
-pub use self::payload::Payload;
+pub use self::payload::{Payload, PayloadStream};
 pub use self::request::Request;
 pub use self::response::Response;
 pub use self::service::{SendError, SendResponse};
