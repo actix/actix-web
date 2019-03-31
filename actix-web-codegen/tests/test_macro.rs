0f072f68def8e8acb744c104f4e43f9eec0ddb94
Douman <douman@gmx.se>
2019-03-31 10:23:15 +0300

    Expand codegen to allow specify guards and async

--- actix-web-codegen/tests/test_macro.rs
+++ actix-web-codegen/tests/test_macro.rs
@@ -1,15 +1,46 @@
 use actix_http::HttpService;
 use actix_http_test::TestServer;
-use actix_web::{get, http, App, HttpResponse, Responder};
+use actix_web_codegen::get;
+use actix_web::{http, App, HttpResponse, Responder};
+use futures::{Future, future};
 
+//fn guard_head(head: &actix_web::dev::RequestHead) -> bool {
+//    true
+//}
+
+//#[get("/test", guard="guard_head")]
 #[get("/test")]
 fn test() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[get("/test")]
+fn auto_async() -> impl Future<Item=HttpResponse, Error=actix_web::Error> {
+    future::ok(HttpResponse::Ok().finish())
+}
+
+#[get("/test")]
+fn auto_sync() -> impl Future<Item=HttpResponse, Error=actix_web::Error> {
+    future::ok(HttpResponse::Ok().finish())
+}
+
+
 #[test]
 fn test_body() {
     let mut srv = TestServer::new(|| HttpService::new(App::new().service(test)));
+    let request = srv.request(http::Method::GET, srv.url("/test"));
+    let response = srv.block_on(request.send()).unwrap();
+    assert!(response.status().is_success());
+
+    let mut srv = TestServer::new(|| HttpService::new(App::new().service(auto_sync)));
+    let request = srv.request(http::Method::GET, srv.url("/test"));
+    let response = srv.block_on(request.send()).unwrap();
+    assert!(response.status().is_success());
+}
+
+#[test]
+fn test_auto_async() {
+    let mut srv = TestServer::new(|| HttpService::new(App::new().service(auto_async)));
 
     let request = srv.request(http::Method::GET, srv.url("/test"));
     let response = srv.block_on(request.send()).unwrap();
