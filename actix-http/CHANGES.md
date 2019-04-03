dfa0abf5a5e158f44bea043b765cdf7625bfa236
Nikolay Kim <fafhrd91@gmail.com>
2019-04-03 12:44:47 -0700

    Export IntoHeaderValue

--- actix-http/CHANGES.md
+++ actix-http/CHANGES.md
@@ -2,6 +2,10 @@
 
 ## [0.1.0-alpha.4] - 2019-04-xx
 
+### Changed
+
+* Export IntoHeaderValue
+
 ### Deleted
 
 * Removed PayloadBuffer
--- src/error.rs
+++ src/error.rs
@@ -121,36 +121,6 @@ impl ResponseError for ReadlinesError {
     }
 }
 
-/// A set of errors that can occur during parsing multipart streams
-#[derive(Debug, Display, From)]
-pub enum MultipartError {
-    /// Content-Type header is not found
-    #[display(fmt = "No Content-type header found")]
-    NoContentType,
-    /// Can not parse Content-Type header
-    #[display(fmt = "Can not parse Content-Type header")]
-    ParseContentType,
-    /// Multipart boundary is not found
-    #[display(fmt = "Multipart boundary is not found")]
-    Boundary,
-    /// Multipart stream is incomplete
-    #[display(fmt = "Multipart stream is incomplete")]
-    Incomplete,
-    /// Error during field parsing
-    #[display(fmt = "{}", _0)]
-    Parse(ParseError),
-    /// Payload error
-    #[display(fmt = "{}", _0)]
-    Payload(PayloadError),
-}
-
-/// Return `BadRequest` for `MultipartError`
-impl ResponseError for MultipartError {
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::new(StatusCode::BAD_REQUEST)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -178,12 +148,6 @@ mod tests {
         let resp: HttpResponse = ReadlinesError::LimitOverflow.error_response();
         assert_eq!(resp.status(), StatusCode::PAYLOAD_TOO_LARGE);
         let resp: HttpResponse = ReadlinesError::EncodingError.error_response();
-        assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
-    }
-
-    #[test]
-    fn test_multipart_error() {
-        let resp: HttpResponse = MultipartError::Boundary.error_response();
         assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
     }
 }
