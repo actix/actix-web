337c2febe34e5d3781076d348c1d732e3035f49b
Nikolay Kim <fafhrd91@gmail.com>
2019-05-02 09:49:10 -0700

    add more tests

--- actix-web-codegen/tests/test_macro.rs
+++ actix-web-codegen/tests/test_macro.rs
@@ -1,12 +1,22 @@
 use actix_http::HttpService;
 use actix_http_test::TestServer;
 use actix_web::{http, App, HttpResponse, Responder};
-use actix_web_codegen::get;
+use actix_web_codegen::{get, post, put};
 use futures::{future, Future};
 
 #[get("/test")]
 fn test() -> impl Responder {
     HttpResponse::Ok()
+}
+
+#[put("/test")]
+fn put_test() -> impl Responder {
+    HttpResponse::Created()
+}
+
+#[post("/test")]
+fn post_test() -> impl Responder {
+    HttpResponse::NoContent()
 }
 
 #[get("/test")]
@@ -21,10 +31,27 @@ fn auto_sync() -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
 
 #[test]
 fn test_body() {
-    let mut srv = TestServer::new(|| HttpService::new(App::new().service(test)));
+    let mut srv = TestServer::new(|| {
+        HttpService::new(
+            App::new()
+                .service(post_test)
+                .service(put_test)
+                .service(test),
+        )
+    });
     let request = srv.request(http::Method::GET, srv.url("/test"));
     let response = srv.block_on(request.send()).unwrap();
     assert!(response.status().is_success());
+
+    let request = srv.request(http::Method::PUT, srv.url("/test"));
+    let response = srv.block_on(request.send()).unwrap();
+    assert!(response.status().is_success());
+    assert_eq!(response.status(), http::StatusCode::CREATED);
+
+    let request = srv.request(http::Method::POST, srv.url("/test"));
+    let response = srv.block_on(request.send()).unwrap();
+    assert!(response.status().is_success());
+    assert_eq!(response.status(), http::StatusCode::NO_CONTENT);
 
     let mut srv = TestServer::new(|| HttpService::new(App::new().service(auto_sync)));
     let request = srv.request(http::Method::GET, srv.url("/test"));
