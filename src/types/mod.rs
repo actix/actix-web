ca612eb28d19a0c9c89040d98cc363796910ea5a
daddinuz <daddinuz@gmail.com>
2019-05-08 23:45:13 +0200

    expose QueryConfig in web module

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -11,4 +11,4 @@ pub use self::form::{Form, FormConfig};
 pub use self::json::{Json, JsonConfig};
 pub use self::path::Path;
 pub use self::payload::{Payload, PayloadConfig};
-pub use self::query::Query;
+pub use self::query::{Query, QueryConfig};
