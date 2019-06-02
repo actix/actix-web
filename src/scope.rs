e6bc864e05c902360f45782c7baf1f6b46aeb27f
Denys Vitali <denys@denv.it>
2019-06-02 20:00:28 +0200

    Scope: Switch to ServiceConfig in doc

--- src/scope.rs
+++ src/scope.rs
@@ -98,7 +98,7 @@ impl Scope {
     /// use actix_web::{web, middleware, App, HttpResponse};
     ///
     /// // this function could be located in different module
-    /// fn config(cfg: &mut web::ScopeConfig) {
+    /// fn config(cfg: &mut web::ServiceConfig) {
     ///     cfg.service(web::resource("/test")
     ///         .route(web::get().to(|| HttpResponse::Ok()))
     ///         .route(web::head().to(|| HttpResponse::MethodNotAllowed()))
