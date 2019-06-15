d7ec241fd081c625551ef07a1039b322935b5821
Nikolay Kim <fafhrd91@gmail.com>
2019-06-15 21:47:06 +0600

    re-export identity and cors middleware

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -11,5 +11,16 @@ pub use self::defaultheaders::DefaultHeaders;
 pub use self::logger::Logger;
 pub use self::normalize::NormalizePath;
 
-//
-// use actix_cors as cors;
+#[cfg(feature = "deprecated")]
+#[deprecated(
+    since = "1.0.1",
+    note = "please use `actix_cors` instead. support will be removed in actix-web 1.0.2"
+)]
+pub use actix_cors as cors;
+
+#[cfg(feature = "deprecated")]
+#[deprecated(
+    since = "1.0.1",
+    note = "please use `actix_identity` instead. support will be removed in actix-web 1.0.2"
+)]
+pub use actix_identity as identity;
