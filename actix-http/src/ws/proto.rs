e4382e4fc126a495baca0e583fd95d7aaee10c0a
Sameer Dhar <sdhar@pm.me>
2019-12-08 23:02:43 -0500

    Fix broken docs (#1204)
    Fixed un escaped brackets in lib.rs, and reflowed links to ConnectionInfo in app, config, and server.rs

--- actix-http/src/ws/proto.rs
+++ actix-http/src/ws/proto.rs
@@ -95,7 +95,7 @@ pub enum CloseCode {
     Abnormal,
     /// Indicates that an endpoint is terminating the connection
     /// because it has received data within a message that was not
-    /// consistent with the type of the message (e.g., non-UTF-8 [RFC3629]
+    /// consistent with the type of the message (e.g., non-UTF-8 \[RFC3629\]
     /// data within a text message).
     Invalid,
     /// Indicates that an endpoint is terminating the connection
