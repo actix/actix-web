897d015ac5280fa506389c20d739788ab14c670f
Jacob Brown <kardeiz@gmail.com>
2020-01-09 15:56:42 -0600

    add `ignore` to doc comment

--- src/request.rs
+++ src/request.rs
@@ -211,7 +211,7 @@ impl HttpRequest {
     ///
     /// If `App::data` was used to store object, use `Data<T>`:
     ///
-    /// ```rust,no_run
+    /// ```rust,ignore
     /// let opt_t = req.app_data::<Data<T>>();
     /// ```
     pub fn app_data<T: 'static>(&self) -> Option<&T> {
