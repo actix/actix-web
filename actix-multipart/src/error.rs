b4047ae05386fbbab9a5f9baee41b66a684fbe61
Otavio Salvador <otavio@ossystems.com.br>
2020-03-11 23:50:22 -0300

    multipart: Convert from `derive_more` to `thiserror`
    The `thiserror` has the advantage of implementing `std::error::Error`
    and it integrates better with the Rust ecosystem.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- actix-multipart/src/error.rs
+++ actix-multipart/src/error.rs
@@ -2,38 +2,36 @@
 use actix_web::error::{ParseError, PayloadError};
 use actix_web::http::StatusCode;
 use actix_web::ResponseError;
-use derive_more::{Display, From};
+use thiserror::Error;
 
 /// A set of errors that can occur during parsing multipart streams
-#[derive(Debug, Display, From)]
+#[derive(Debug, Error)]
 pub enum MultipartError {
     /// Content-Type header is not found
-    #[display(fmt = "No Content-type header found")]
+    #[error("No Content-type header found")]
     NoContentType,
     /// Can not parse Content-Type header
-    #[display(fmt = "Can not parse Content-Type header")]
+    #[error("Can not parse Content-Type header")]
     ParseContentType,
     /// Multipart boundary is not found
-    #[display(fmt = "Multipart boundary is not found")]
+    #[error("Multipart boundary is not found")]
     Boundary,
     /// Nested multipart is not supported
-    #[display(fmt = "Nested multipart is not supported")]
+    #[error("Nested multipart is not supported")]
     Nested,
     /// Multipart stream is incomplete
-    #[display(fmt = "Multipart stream is incomplete")]
+    #[error("Multipart stream is incomplete")]
     Incomplete,
     /// Error during field parsing
-    #[display(fmt = "{}", _0)]
-    Parse(ParseError),
+    #[error(transparent)]
+    Parse(#[from] ParseError),
     /// Payload error
-    #[display(fmt = "{}", _0)]
-    Payload(PayloadError),
+    #[error(transparent)]
+    Payload(#[from] PayloadError),
     /// Not consumed
-    #[display(fmt = "Multipart stream is not consumed")]
+    #[error("Multipart stream is not consumed")]
     NotConsumed,
 }
-
-impl std::error::Error for MultipartError {}
 
 /// Return `BadRequest` for `MultipartError`
 impl ResponseError for MultipartError {
