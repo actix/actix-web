3527132c4d6fdb795d3c54b51084764738055e39
Matt Gathu <mattgathu@gmail.com>
2020-09-15 21:30:26 +0200

    Support stable versions in tests

--- actix-web-codegen/tests/trybuild.rs
+++ actix-web-codegen/tests/trybuild.rs
@@ -1,4 +1,3 @@
-#[rustversion::attr(not(stable(1.42)), ignore)]
 #[test]
 fn compile_macros() {
     let t = trybuild::TestCases::new();
@@ -7,7 +6,22 @@ fn compile_macros() {
     t.compile_fail("tests/trybuild/simple-fail.rs");
 
     t.pass("tests/trybuild/route-ok.rs");
-    t.compile_fail("tests/trybuild/route-missing-method-fail.rs");
     t.compile_fail("tests/trybuild/route-duplicate-method-fail.rs");
     t.compile_fail("tests/trybuild/route-unexpected-method-fail.rs");
+
+    test_route_missing_method(&t)
 }
+
+#[rustversion::stable(1.42)]
+fn test_route_missing_method(t: &trybuild::TestCases) {
+    t.compile_fail("tests/trybuild/route-missing-method-fail-msrv.rs");
+}
+
+#[rustversion::not(stable(1.42))]
+#[rustversion::not(nightly)]
+fn test_route_missing_method(t: &trybuild::TestCases) {
+    t.compile_fail("tests/trybuild/route-missing-method-fail.rs");
+}
+
+#[rustversion::nightly]
+fn test_route_missing_method(_t: &trybuild::TestCases) {}
