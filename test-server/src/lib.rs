a429ee66466c8ddb1f6c922dfc9b0758b22d5815
Aleksandrov Vladimir <invis87@gmail.com>
2020-09-15 14:09:16 +0300

    Add possibility to set address for test_server (#1645)

--- test-server/src/lib.rs
+++ test-server/src/lib.rs
@@ -44,12 +44,20 @@ pub use actix_testing::*;
 /// }
 /// ```
 pub async fn test_server<F: ServiceFactory<TcpStream>>(factory: F) -> TestServer {
+    let tcp = net::TcpListener::bind("127.0.0.1:0").unwrap();
+    test_server_with_addr(tcp, factory).await
+}
+
+/// Start [`test server`](./fn.test_server.html) on a concrete Address
+pub async fn test_server_with_addr<F: ServiceFactory<TcpStream>>(
+    tcp: net::TcpListener,
+    factory: F,
+) -> TestServer {
     let (tx, rx) = mpsc::channel();
 
     // run server in separate thread
     thread::spawn(move || {
         let sys = System::new("actix-test-server");
-        let tcp = net::TcpListener::bind("127.0.0.1:0").unwrap();
         let local_addr = tcp.local_addr().unwrap();
 
         Server::build()
