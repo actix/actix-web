5305fc76648471ca6ec467910ff5566712988664
Rob Ede <robjtede@icloud.com>
2021-04-22 15:28:13 +0100

    clippy

--- actix-http/src/body/message_body.rs
+++ actix-http/src/body/message_body.rs
@@ -82,7 +82,7 @@ where
         cx: &mut Context<'_>,
     ) -> Poll<Option<Result<Bytes, Self::Error>>> {
         match ready!(self.as_mut().poll_next(cx)) {
-            Some(Err(err)) => Poll::Ready(Some(Err(err.into()))),
+            Some(Err(err)) => Poll::Ready(Some(Err(err))),
             Some(Ok(val)) => Poll::Ready(Some(Ok(val))),
             None => Poll::Ready(None),
         }
