d6a09f0bedca9ca35b1aca673408c97862ca73c6
edgerunnergit <edgerunnergit@riseup.net>
2023-01-31 08:14:41 +0530

    fixed a test - that should be able to pass

--- actix-web-codegen/tests/trybuild/route-custom-method.rs
+++ actix-web-codegen/tests/trybuild/route-custom-method.rs
@@ -1,4 +1,6 @@
 use actix_web_codegen::*;
+use actix_web::http::Method;
+use std::str::FromStr;
 
 #[route("/", method="UNEXPECTED")]
 async fn index() -> String {
@@ -11,7 +13,7 @@ async fn main() {
 
     let srv = actix_test::start(|| App::new().service(index));
 
-    let request = srv.get("/");
+    let request = srv.request(Method::from_str("UNEXPECTED").unwrap(), srv.url("/"));
     let response = request.send().await.unwrap();
     assert!(response.status().is_success());
 }
