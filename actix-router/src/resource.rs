d22c9f9fb12db30ee1cb7183ce8cf395c707f30d
Rob Ede <robjtede@icloud.com>
2023-07-20 10:49:01 +0100

    update syn to 2 in web codegen (#3081)

--- actix-router/src/resource.rs
+++ actix-router/src/resource.rs
@@ -252,7 +252,7 @@ impl ResourceDef {
     /// Multi-pattern resources can be constructed by providing a slice (or vec) of patterns.
     ///
     /// # Panics
-    /// Panics if path pattern is malformed.
+    /// Panics if any path patterns are malformed.
     ///
     /// # Examples
     /// ```
@@ -838,6 +838,7 @@ impl ResourceDef {
 
     fn construct<T: IntoPatterns>(paths: T, is_prefix: bool) -> Self {
         let patterns = paths.patterns();
+
         let (pat_type, segments) = match &patterns {
             Patterns::Single(pattern) => ResourceDef::parse(pattern, is_prefix, false),
 
