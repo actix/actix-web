a94b5b89a566bdce2c727beb9b710634202af4c2
asonix <asonix@asonix.dog>
2024-05-19 16:52:59 -0500

    Add other endpoints to actix web example

--- actix-http/examples/actix-web.rs
+++ actix-http/examples/actix-web.rs
@@ -1,11 +1,25 @@
+use std::sync::OnceLock;
+
 use actix_http::HttpService;
 use actix_server::Server;
 use actix_service::map_config;
 use actix_web::{dev::AppConfig, get, App};
 
+static LARGE: OnceLock<String> = OnceLock::new();
+
 #[get("/")]
 async fn index() -> &'static str {
     "Hello, world. From Actix Web!"
+}
+
+#[get("/large")]
+async fn large() -> &'static str {
+    LARGE.get_or_init(|| "123456890".repeat(1024 * 10))
+}
+
+#[get("/medium")]
+async fn medium() -> &'static str {
+    LARGE.get_or_init(|| "123456890".repeat(1024 * 5))
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -13,7 +27,7 @@ async fn main() -> std::io::Result<()> {
     Server::build()
         .bind("hello-world", "127.0.0.1:8080", || {
             // construct actix-web app
-            let app = App::new().service(index);
+            let app = App::new().service(index).service(large).service(medium);
 
             HttpService::build()
                 // pass the app to service builder
