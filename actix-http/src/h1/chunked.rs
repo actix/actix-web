c1357eb66f5b24fd4e677ef5204f21dbc998a9e0
Rob Ede <robjtede@icloud.com>
2026-09-25 18:59:51 +0100

    chore(actix-http): prepare release 3.18.9

--- actix-http/src/h1/chunked.rs
+++ actix-http/src/h1/chunked.rs
@@ -17,8 +17,8 @@ macro_rules! byte (
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(super) enum ChunkedState {
-    Size,
-    SizeMore,
+    SizeFirst,
+    SizeRest,
     SizeLws,
     Extension,
     SizeLf,
@@ -39,8 +39,8 @@ impl ChunkedState {
     ) -> Poll<Result<ChunkedState, io::Error>> {
         use self::ChunkedState::*;
         match *self {
-            Size => ChunkedState::read_size(body, size),
-            SizeMore => ChunkedState::read_size_more(body, size),
+            SizeFirst => ChunkedState::read_size_first(body, size),
+            SizeRest => ChunkedState::read_size_rest(body, size),
             SizeLws => ChunkedState::read_size_lws(body),
             Extension => ChunkedState::read_extension(body),
             SizeLf => ChunkedState::read_size_lf(body, *size),
@@ -53,27 +53,28 @@ impl ChunkedState {
         }
     }
 
-    fn read_size(rdr: &mut BytesMut, size: &mut u64) -> Poll<Result<ChunkedState, io::Error>> {
-        let b = byte!(rdr);
-        if !b.is_ascii_hexdigit() {
-            return Poll::Ready(Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "400 Bad Request",
-            )));
-        }
-
-        let rem = match b {
-            b'0'..=b'9' => b - b'0',
-            b'a'..=b'f' => b + 10 - b'a',
-            b'A'..=b'F' => b + 10 - b'A',
-            _ => unreachable!(),
+    fn read_size_first(
+        rdr: &mut BytesMut,
+        size: &mut u64,
+    ) -> Poll<Result<ChunkedState, io::Error>> {
+        let rem = match byte!(rdr) {
+            b @ b'0'..=b'9' => b - b'0',
+            b @ b'a'..=b'f' => b + 10 - b'a',
+            b @ b'A'..=b'F' => b + 10 - b'A',
+            _ => {
+                return Poll::Ready(Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Invalid chunk size line: Invalid size first-byte",
+                )))
+            }
         };
 
         *size = rem as u64;
-        Poll::Ready(Ok(ChunkedState::SizeMore))
+
+        Poll::Ready(Ok(ChunkedState::SizeRest))
     }
 
-    fn read_size_more(rdr: &mut BytesMut, size: &mut u64) -> Poll<Result<ChunkedState, io::Error>> {
+    fn read_size_rest(rdr: &mut BytesMut, size: &mut u64) -> Poll<Result<ChunkedState, io::Error>> {
         let radix = 16;
 
         let rem = match byte!(rdr) {
@@ -86,7 +87,7 @@ impl ChunkedState {
             _ => {
                 return Poll::Ready(Err(io::Error::new(
                     io::ErrorKind::InvalidInput,
-                    "Invalid chunk size line: Invalid Size",
+                    "Invalid chunk size line: Invalid size",
                 )));
             }
         };
@@ -96,7 +97,7 @@ impl ChunkedState {
                 *size = n;
                 *size += rem as u64;
 
-                Poll::Ready(Ok(ChunkedState::SizeMore))
+                Poll::Ready(Ok(ChunkedState::SizeRest))
             }
             None => {
                 debug!("chunk size would overflow u64");
@@ -181,7 +182,7 @@ impl ChunkedState {
     }
     fn read_body_lf(rdr: &mut BytesMut) -> Poll<Result<ChunkedState, io::Error>> {
         match byte!(rdr) {
-            b'\n' => Poll::Ready(Ok(ChunkedState::Size)),
+            b'\n' => Poll::Ready(Ok(ChunkedState::SizeFirst)),
             _ => Poll::Ready(Err(io::Error::new(
                 io::ErrorKind::InvalidInput,
                 "Invalid chunk body LF",
